d60a512422380a4d88b290216a6ec94685887dd2
caugustocc <cronuz300@gmail.com>
2022-06-04 14:02:00 -0500

    c15 Agregando ICO y resolviendo HBS

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(_, res) {
-  res.render('index', { 
+router.get('/', (_, res) => {
+  res.render('index', {
     title: 'SAV',
-    author: 'Oaxacarlos'
+    author: 'Oaxacarlos',
   });
 });
 
