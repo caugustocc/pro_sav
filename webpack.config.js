5f276319ce351aee432e4b72189f24e8e8cca81d
caugustocc <cronuz300@gmail.com>
2022-06-03 21:44:19 -0500

    c7 corrigiendo webpack (verificar)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require ('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './client/index.js',   
     output:{
@@ -6,4 +7,36 @@ module.exports = {
         filename: path.join('javascript','bundle.js'),
         publicPath: '/',
     },
+    module: {
+        rules: [
+            {
+                 test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env', {
+                                        modules: false,
+                                        useBuiltIns: 'usage',
+                                        targets: '> 0.25%, not dead',
+                                        corejs: 3
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader]
+            }
+        ]
+    },
+    Plugins: [new MiniCssExtractPlugin({
+        filename:path.join('stylesheets', 'styles.css')
+    })]
 }
